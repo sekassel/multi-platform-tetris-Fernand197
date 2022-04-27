9d36380fba818109570c3b1921a16fc995c8697f
Fernand197 <70778077+Fernand197@users.noreply.github.com>
2022-04-27 14:04:07 +0100

    Delete cross-platform-monorepo directory

--- cross-platform-monorepo/projects/game-engine-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of game-engine-lib
- */
-
-export * from './lib/game-engine-lib.service';
-export * from './lib/game-engine-lib.component';
-export * from './lib/game-engine-lib.module';
