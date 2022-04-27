aca59021a5e661b7577d6a81c9dc9d66eac2fcf6
Fernand197 <tatchumf@gmail.com>
2022-04-27 14:29:31 +0100

    init

---
+++ cross-platform-monorepo/projects/game-engine-lib/src/lib/game-engine-lib.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BoardComponent } from './components/board/board.component';
+import { GameEngineLibComponent } from './game-engine-lib.component';
+
+
+@NgModule({
+  declarations: [
+    GameEngineLibComponent,
+    BoardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    GameEngineLibComponent,
+    BoardComponent
+  ]
+})
+export class GameEngineLibModule { }
